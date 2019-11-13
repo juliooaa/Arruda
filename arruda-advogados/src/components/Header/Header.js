68cd4343232dc7bc1df32a2830db9ea4fb765359
julio <julimaraujo93@gmail.com>
2019-11-13 12:24:43 -0300

    Header Begin

---
+++ arruda-advogados/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./styles.css"
+
+function Header() {
+    return (
+        <div className="headerWrapper">
+            Testeeeeeeee
+        </div>
+    )
+}
+
+export default Header
