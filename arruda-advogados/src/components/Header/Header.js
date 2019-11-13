1003a7508547bea3216c9f5aa1b1f0739f66c8aa
julio <julimaraujo93@gmail.com>
2019-11-13 16:36:02 -0300

    Header almost done

--- arruda-advogados/src/components/Header/Header.js
+++ arruda-advogados/src/components/Header/Header.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { Icon } from "@blueprintjs/core";
 
 import "./styles.css"
 
 function Header() {
     return (
         <div className="headerWrapper">
-            Testeeeeeeee
+            <ul className="contentList">
+                <li>
+                    <Icon icon="calendar" iconSize={17} color="#ACACAC" />
+                    <p>Agenda</p>
+                </li>
+                <li>
+                    <Icon icon= "people" iconSize={17} color="#ACACAC" />
+                    <p>Usuários</p>
+                </li>
+                <li>
+                    <Icon icon= "user" iconSize={17} color="#ACACAC" />
+                    <p>Pessoas</p>
+                </li>
+                <li>
+                    <Icon icon= "document" iconSize={17} color="#ACACAC" />
+                    <p>Casos</p>
+                </li>
+                <li>
+                    <Icon icon= "box" iconSize={17} color="#ACACAC" />
+                    <p>Documentações</p>
+                </li>
+            </ul>
+            <div class="contentSearch">
+                <input type="search" placeholder="Pesquisar na plataforma..." dir="auto" />
+                <button>
+                    <Icon icon="search" iconSize={20} />
+                </button>
+            </div>
         </div>
     )
 }
