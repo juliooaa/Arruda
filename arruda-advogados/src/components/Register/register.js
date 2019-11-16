2c88d2da58729fb6f975d08449c0306642101f7f
julio <julimaraujo93@gmail.com>
2019-11-16 20:39:21 -0300

    Register Ok

--- arruda-advogados/src/components/Register/register.js
+++ arruda-advogados/src/components/Register/register.js
@@ -5,41 +5,25 @@ import { Label } from "@blueprintjs/core";
 import "./styles.css"
 
 function registerPerson() {
+
+    const inputs = ['Nome*', 'Cpf', 'RG', 'Email', 'Profissão', 'Estado civil', 'Nacionalidades*', 'Conjugue', 'Informações adicionais...'];
+
+    const listInputs = inputs.map((value) =>
+        <Label>
+            <input className={Classes.INPUT} placeholder={value} />
+        </Label>
+    );
+
     return(
         <div className="container">
             <h1>Cadastrar:</h1>
+            
             <div className="buttons">
                 <button type="button">Button</button>
                 <button type="button">Pessoas físicas</button>
             </div>
             <div className="form">
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Nome*" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Cpf" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="RG" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Email" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Profissão" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Estado civil" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Nacionalidade*" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Conjugue" />
-                </Label>
-                <Label>
-                    <input className={Classes.INPUT} placeholder="Informações adicionais..." />
-                </Label>
+                {listInputs}
             </div>
             <button Icon="arrow-right" type="button" text="Next step" />
         </div>
