96401f461d0c96239b39813e3abc6dee84afa5e3
julio <julimaraujo93@gmail.com>
2019-11-19 15:12:26 -0300

    Adjustment NavBar

--- arruda-advogados/src/App.tsx
+++ arruda-advogados/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './components/Header/header';
 import Register from './components/Register/register';
-import Menu from './components/Lateral_Menu/lateralMenu';
+import Menu from './components/Lateral_Menu/navBar';
 
 const App: React.FC = () => {
   return (
--- arruda-advogados/src/components/Lateral_Menu/navBar.js
+++ arruda-advogados/src/components/Lateral_Menu/navBar.js
@@ -10,7 +10,7 @@ function menu() {
     );
     
     return(
-        <div className="options">
+        <div className="content">
             <ul>
                 <li>
                     {listOptions}
